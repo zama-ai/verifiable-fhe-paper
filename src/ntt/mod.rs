8f6ad02444ff3a7e43e41e9013f29aa3b5dfb1c6
Michael Walter <michael.walter@zama.ai>
2024-03-08 14:28:18 +0100

    add README

--- src/ntt/mod.rs
+++ src/ntt/mod.rs
@@ -4,7 +4,8 @@ use plonky2::iop::target::Target;
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 
 // use this path to set the ring dimension N (i.e. for N=512 set the path to "params_512.rs")
-// make sure to adjust the value of circuit_size in line 57 of "ivc_based_vpbs.rs"
+// make sure to adjust the value of circuit_size in line 57 of "ivc_based_vpbs.rs" if
+// using a value other than 8 or 1024
 #[path = "params_1024.rs"]
 pub mod params;
 
